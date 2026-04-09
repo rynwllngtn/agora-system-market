cc2f2f87c17b6c5bce4eb7d45882329ff2ece440
Ryan Wellington <rynwllngtn@gmail.com>
2026-04-09 12:38:38 -0300

    init

---
+++ src/test/java/dev/rynwllngtn/agorasytem/AgoraSystemMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rynwllngtn.agorasytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraSystemMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
